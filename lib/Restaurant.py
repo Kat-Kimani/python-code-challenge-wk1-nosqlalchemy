4ee10b02a92f5d491454af967b22933f50baa3e8
catherine <wachukimani@gmail.com>
2023-05-30 21:34:37 +0300

    orm review,rest and customer

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,8 +1,17 @@
 class Restaurant:
     def __init__(self, name):
         self._name=name
+        self._reviews=[]
     def name(self):
-        return self._name 
+        return self._name
+    def reviews(self):
+        return self._reviews
+    def customers(self):
+        customer_list=set()
+        for review in self._reviews:
+            customer_list.add(review.customer())
+        return list(customer_list)
+        
 restaurant= Restaurant("Fenty")
 print(restaurant.name())
    
--- lib/Review.py
+++ lib/Review.py
@@ -3,12 +3,16 @@ from Restaurant import Restaurant
 class Review:
     all_reviews=[]
     def __init__(self, customer, restaurant, rating ):
-        self.customer= customer
-        self.restaurant= restaurant
+        self._customer= customer
+        self._restaurant= restaurant
         self.rating = rating
         Review.all_reviews.append(self)
     def get_rating(self):
         return self.rating
+    def customer(self):
+        return self._customer
+    def restaurant(self):
+        return self._restaurant
     @classmethod
     def get_all_reviews(cls):
         return cls.all_reviews
